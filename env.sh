1b5c77b6bc782e766ab151fe6a9cb5cc28a9f5bf
Jonathan Shook <jshook@gmail.com>
2015-10-26 09:41:45 -0500

    minor fixes

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ then
  export PATH=$PATH:${FIO_TESTDIR}/bin
  export FIO_RESULTS=${FIO_TESTDIR}/results
  export FIO_CONFDIR=${FIO_TESTDIR}/conf
- export FIO_DATADIR=/mnt/data
+ export FIO_DATADIR=change_me
 else 
  echo "you must be in the fiotest directory to source this file."
 fi
